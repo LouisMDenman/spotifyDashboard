250d716c5688fe2b36ffedafed68f970a1b8c1dd
Louis Denman <lmdenman02@gmail.com>
2024-10-14 20:03:46 +1000

    potential redirect_uri bug fix for page not returning stats

--- src/contexts/spotifyAuthProvider.jsx
+++ src/contexts/spotifyAuthProvider.jsx
@@ -45,7 +45,7 @@ export function SpotifyAuthProvider({children}){
         params.append("client_id", clientId)
         params.append("grant_type", "authorization_code")
         params.append("code", code)
-        params.append("redirect_uri", "http://localhost:5173/spotifycallback")
+        params.append("redirect_uri", import.meta.env.VITE_SPOTIFY_CALLBACK)
         params.append("code_verifier", verifier)
 
         const result = await fetch("https://accounts.spotify.com/api/token", {
